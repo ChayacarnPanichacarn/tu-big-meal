231e0a456c58880cc5e641e844b3df90072c81c5
ChayacarnPanichacarn <115107350+ChayacarnPanichacarn@users.noreply.github.com>
2024-12-08 04:41:21 +0700

    Update db.js

--- tuBigMeal_server/db.js
+++ tuBigMeal_server/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const connectDB = async () => {
     try{
         const mongoURI = process.env.MONGO_URI;
-        const conn = await mongoose.connect(mongoURI);
+        const conn = await mongoose.connect(process.env.mongoURI);
         console.log(`MongoDB Connected`)
     }
     catch(error){
