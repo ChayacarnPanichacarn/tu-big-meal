cc778fbd7a371024e240d1346cfa001c3f582b1d
ChayacarnPanichacarn <gigiedevil@gmail.com>
2024-12-09 02:39:27 +0700

    Modified google service account key of env in index.js

--- tuBigMeal_server/index.js
+++ tuBigMeal_server/index.js
@@ -119,9 +119,26 @@ const server_storage = multer.diskStorage({
     }
 })
 
-const serviceAccountKey = process.env.GCS_KEY;
+// const serviceAccountKey = JSON.parse(process.env.GCS_KEY);
+// const serviceAccountKey = process.env.GCS_KEY;
+
+const serviceAccountKey = {
+    "type": "service_account",
+    "project_id": "tu-big-meal",
+    "private_key_id": process.env.private_key_id,
+    "private_key": process.env.private_key,
+    "client_email": process.env.client_email,
+    "client_id": process.env.client_id,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/tubigmealserviceaccount%40tu-big-meal.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+  };
+
 const gcs = new Storage({
     credentials: serviceAccountKey,
+    // keyFilename: './gcs_key.json'
 })
 
 const bucketName = "tubigmealgcsbucket";
